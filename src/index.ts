493f2f4b0f8a0b3fe3ce57886327adc8aba84cd6
Adam Mahood <mahood.adam@gmail.com>
2021-03-22 19:13:30 -0400

    hotfix/v1.0.0: fixing exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-export * from "./lib/connection_pool/ConnectionPool";
-export * from "./lib/query/Query";
-export * from "./lib/query/query_builders";
-export * from "./lib/query/query_mods";
+import ConnectionPool from "./lib/connection_pool/ConnectionPool";
+import Query from "./lib/query/Query";
+import {
+  limit,
+  offset,
+  or,
+  orderBy,
+  where,
+} from "./lib/query/query_mods";
+
+export {
+  ConnectionPool,
+  Query,
+  limit,
+  offset,
+  or,
+  orderBy,
+  where,
+};
